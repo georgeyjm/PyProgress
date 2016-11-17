a71a45f2484131e9c56ed53b3664fa0f713a3512
George Yu <dumpling_yu@163.com>
2016-11-17 13:36:23 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,9 +4,6 @@ import time
 count = 12
 
 progressBar = pyprogress.bar(count)
-progressBar.setup()
 for i in range(count):
     time.sleep(0.5) #Work Here
     progressBar.update()
-
-progressBar.finish()
