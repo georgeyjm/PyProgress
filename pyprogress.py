cf54f95336f31b4ecf5e7fce2a5fbf03fa79a0fd
George Yu <dumpling_yu@163.com>
2017-01-10 18:16:36 +0800

    Added in the exception for too many updates.

--- pyprogress.py
+++ pyprogress.py
@@ -2,6 +2,9 @@ import random
 import time
 import sys
 
+class UpdateError(Exception):
+    pass
+
 class bar(object):
     def __init__(self, count, width=50):
         self.amount = 0
@@ -13,6 +16,8 @@ class bar(object):
         sys.stdout.write('\b'*(self.width+1))
 
     def update(self):
+        if self.done == self.count:
+            raise UpdateError('too much updates!')
         self.amount %= self.count
         self.amount += self.width
         self.length = self.amount // self.count
